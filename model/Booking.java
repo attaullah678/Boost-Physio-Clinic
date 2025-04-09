488cef49858201ece04de07eddb07a4fd2d3699d
attaullah678 <attaullah.official678@gmail.com>
2025-04-10 00:10:19 +0500

    Update Booking.java
    Added getters and setters for Booking

--- model/Booking.java
+++ model/Booking.java
@@ -6,12 +6,47 @@ public class Booking {
     private Treatment treatment;
     private String status; // "booked", "attended", "cancelled"
 
-    // Constructor
     public Booking(String bookingId, Patient patient, Treatment treatment, String status) {
         this.bookingId = bookingId;
         this.patient = patient;
         this.treatment = treatment;
         this.status = status;
     }
+
+    // Getters and Setters
+    public String getBookingId() {
+        return bookingId;
+    }
+    public void setBookingId(String bookingId) {
+        this.bookingId = bookingId;
+    }
+
+    public Patient getPatient() {
+        return patient;
+    }
+    public void setPatient(Patient patient) {
+        this.patient = patient;
+    }
+
+    public Treatment getTreatment() {
+        return treatment;
+    }
+    public void setTreatment(Treatment treatment) {
+        this.treatment = treatment;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public void cancelBooking() {
+        this.status = "cancelled";
+    }
+
+    
+
 
 }
